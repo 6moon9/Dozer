eda531a5db71d9bd6106a06abb5f75d64a73b8de
Simon Pucheu <104694344+6moon9@users.noreply.github.com>
2022-11-29 19:40:36 +0100

    Update

--- src/Dozer/Dozer.ino
+++ src/Dozer/Dozer.ino
@@ -1,87 +1,32 @@
 #include <Bluetooth.h>
 #include <Mecanum.h>
 #include <Report.h>
-#include <BlackLineSensor.h>
-#include <HCSR04.h>
+//#include <BlackLineSensor.h>
+//#include <HCSR04.h>
 
 #define loopTime 100
 
-Mecanum mecanum;
+//                   left             right
+//                top    bottom   top    bottom
+Mecanum mecanum(10, 11,   8, 9,   2, 3,   4, 5);
 
-struct
-{
-    void left (int speed)
-    {
-        mecanum.motors[Left][Top].backward(speed);
-        mecanum.motors[Left][Bottom].forward(speed);
-        mecanum.motors[Right][Top].backward(speed);
-        mecanum.motors[Right][Bottom].forward(speed);
-    }
-    void right (int speed)
-    {
-        mecanum.motors[Left][Top].forward(speed);
-        mecanum.motors[Left][Bottom].backward(speed);
-        mecanum.motors[Right][Top].forward(speed);
-        mecanum.motors[Right][Bottom].backward(speed);
-    }
-} sideway;
-struct
-{
-    void forward (int speed)
-    {
-        mecanum.motors[Left][Top].forward(speed);
-        mecanum.motors[Left][Bottom].stop();
-        mecanum.motors[Right][Top].stop();
-        mecanum.motors[Right][Bottom].forward(speed);    
-    }
-    void backward (int speed)
-    {
-        mecanum.motors[Left][Top].stop();
-        mecanum.motors[Left][Bottom].forward(speed);
-        mecanum.motors[Right][Top].forward(speed);
-        mecanum.motors[Right][Bottom].stop();
-    }
-} diagonal;
-struct Mecaside
-{
-    public:
-        Mecaside(int iSide)
-        {
-            side = iSide;
-        }
-        int side = 0;
-        void forward(int speed)
-        {
-            mecanum.motors[side][0].forward(speed);
-            mecanum.motors[side][1].forward(speed);
-        }
-        void backward(int speed)
-        {
-            mecanum.motors[side][0].backward(speed);
-            mecanum.motors[side][1].backward(speed);
-        }
-        void stop()
-        {
-            mecanum.motors[side][0].stop();
-            mecanum.motors[side][1].stop();
-        }
-};
-
-/* Warning! All the pin numbers are fakes and are for compilation testing only. For real upload, please change them with the real ones. */
+#include "ExtraMeca.h"
 
 Bluetooth bluetooth(&Serial1);
 Mecaside left(Left);
 Mecaside right(Right);
 Report report(&Serial, true, 5 * (1000 / loopTime));
-BlackLineSensor blackLine(A0, A1, A2);
-HCSR04 backDistance(2, 3);
+//BlackLineSensor blackLine(A0, A1, A2);
+//HCSR04 backDistance(2, 3);
+
+//void (*findTheLine)() // An void pointer function, can be an option for the autopilot. The other option is a class: AutoPilot autopilot; autopilot.findTheLine();
 
 void setup ()
 {
   // Serial setup //
   {
     Serial1.begin(9600);
-    Serial.begin(9600);
+    Serial.begin(9600); 
     Serial.println("Computer serial communication's on...");
   }
   // Mecanum setup //
@@ -97,49 +42,15 @@ void loop ()
     if (bluetooth.lastError == DeserializationError::Ok)
     {
       report.ok++;
-      serializeJson(bluetooth.json, Serial);
+      //serializeJson(bluetooth.json, Serial);
       bool motors = true;
       // AutoPilot //
       {
-        void findTheLineToTheLeft ()
-        {
-          motors = false;
-          mecanum.sideway.left(512);
-          while (blackLine.getPattern != Position.Pattern.OnTheLine);
-          mecanum.stop();
-        }
-        void findTheLineToTheRight ()
-        {
-          motors = false;
-          mecanum.sideway.right(512);
-          while (blackLine.getPattern != Position.Pattern.OnTheLine);
-          mecanum.stop();
-        }
-        void goBackToCherry ()
-        {
-          motors = false;
-          mecanum.backward(512);
-          while (blackLine.getPattern() == Position.Pattern.OnTheLine && backDistance.getValue() != -1);
-          if (blackLine.getPattern() != Position.Pattern.OnTheLine)
-          {
-            if (blackLine.getPattern() == Position.Pattern.OnTheRight)
-            {
-              mecanum.left.backward(512);
-              mecanum.right.forward(512);
-              while (blackLine.getPattern() != Position.Pattern.OnTheLine);
-              mecanum.stop();
-              goBackToCherry();
-            }
-          }
-          else
-          {
-            mecanum.stop();
-          }
-        }
+        
       }
-      // Keybull //
+      /*// Keybull //
       {
-        switch (bluetooth.json["keybull"])
+        switch (bluetooth.json["keybull"].as<int>())
         {
           case 1:
             findTheLineToTheRight();
@@ -153,7 +64,7 @@ void loop ()
             break;
 
         }
-      }
+      }*/
       // Motors //
       {
         if (!motors) return;
@@ -242,8 +153,8 @@ void loop ()
       // Response //
       {
         bluetooth.json.clear();
-        bluetooth.json["blackLine"]["pattern"] = blackLine.getPattern();
-        bluetooth.json["blackLine"]["onTheLine"] = blackLine.lastPattern() == Position.Pattern.OnTheLine;
+//        bluetooth.json["blackLine"]["pattern"] = blackLine.getPattern();
+//        bluetooth.json["blackLine"]["onTheLine"] = blackLine.lastPattern == Position.Pattern.OnTheLine;
         bluetooth.send();
       }
     }
@@ -260,3 +171,39 @@ void loop ()
   report.print();
   delay(loopTime);
 }
+/*
+void findTheLineToTheLeft ()
+{
+  motors = false;
+  mecanum.sideway.left(512);
+  while (blackLine.getPattern != Position.Pattern.OnTheLine);
+  mecanum.stop();
+}
+void findTheLineToTheRight ()
+{
+  motors = false;
+  mecanum.sideway.right(512);
+  while (blackLine.getPattern != Position.Pattern.OnTheLine);
+  mecanum.stop();
+}
+void goBackToCherry ()
+{
+  motors = false;
+  mecanum.backward(512);
+  while (blackLine.getPattern() == Position.Pattern.OnTheLine && backDistance.getValue() != -1);
+  if (blackLine.getPattern() != Position.Pattern.OnTheLine)
+  {
+    if (blackLine.getPattern() == Position.Pattern.OnTheRight)
+    {
+      mecanum.left.backward(512);
+      mecanum.right.forward(512);
+      while (blackLine.getPattern() != Position.Pattern.OnTheLine);
+      mecanum.stop();
+      goBackToCherry();
+    }
+  }
+  else
+  {
+    mecanum.stop();
+  }
+}*/
---
+++ src/Dozer/ExtraMeca.h
@@ -0,0 +1,58 @@
+struct
+{
+    void left (int speed)
+    {
+        mecanum.motors[Left][Top].backward(speed);
+        mecanum.motors[Left][Bottom].forward(speed);
+        mecanum.motors[Right][Top].backward(speed);
+        mecanum.motors[Right][Bottom].forward(speed);
+    }
+    void right (int speed)
+    {
+        mecanum.motors[Left][Top].forward(speed);
+        mecanum.motors[Left][Bottom].backward(speed);
+        mecanum.motors[Right][Top].forward(speed);
+        mecanum.motors[Right][Bottom].backward(speed);
+    }
+} sideway;
+struct
+{
+    void forward (int speed)
+    {
+        mecanum.motors[Left][Top].forward(speed);
+        mecanum.motors[Left][Bottom].stop();
+        mecanum.motors[Right][Top].stop();
+        mecanum.motors[Right][Bottom].forward(speed);    
+    }
+    void backward (int speed)
+    {
+        mecanum.motors[Left][Top].stop();
+        mecanum.motors[Left][Bottom].forward(speed);
+        mecanum.motors[Right][Top].forward(speed);
+        mecanum.motors[Right][Bottom].stop();
+    }
+} diagonal;
+class Mecaside
+{
+    public:
+        Mecaside(int iSide)
+        {
+            side = iSide;
+        }
+        int side = 0;
+        void forward(int speed)
+        {
+            mecanum.motors[side][0].forward(speed);
+            mecanum.motors[side][1].forward(speed);
+        }
+        void backward(int speed)
+        {
+            mecanum.motors[side][0].backward(speed);
+            mecanum.motors[side][1].backward(speed);
+        }
+        void stop()
+        {
+            mecanum.motors[side][0].stop();
+            mecanum.motors[side][1].stop();
+        }
+};
